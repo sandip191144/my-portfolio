7787ab399ac1ad5a6064d22453176b25806e11b6
sandip191144 <sandipshrimangale541@gmail.com>
2026-02-05 11:15:20 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/My-new-Portfolio/',
+  base: '/my-portfolio/',
   plugins: [react(),tailwindcss(),],
 })
